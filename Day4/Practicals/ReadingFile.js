924be06653aaec987c1f70586252772c3a925fdb
DanielMagar <magar.t.daniel@gmail.com>
2025-04-24 11:15:12 +0530

    Sync Vs Async FS

---
+++ Day4/Practicals/ReadingFile.js
@@ -0,0 +1,14 @@
+// ✅ 1. Synchronous File Reading – fs.readFileSync
+
+const fs = require("fs");
+
+try {
+  const data = fs.readFileSync("file.txt", "utf8");
+  console.log("File content:\n", data);
+} catch (error) {
+  console.log("Error reading file:", error);
+}
+//🔍 Notes:
+// 1.'utf8' is important — it ensures the content is returned as a string, not a Buffer.
+
+// 2. If the file doesn't exist, this will throw an error — so wrap it in a try-catch if needed.
